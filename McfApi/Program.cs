0729947ab31f617ae95082857a7eb4c68ae1aff6
Asep Maulana <pg32@adiconsulting.co>
2024-11-22 23:21:59 +0700

    init environtment variabel

--- McfApi/Program.cs
+++ McfApi/Program.cs
@@ -9,18 +9,33 @@ using System;
 Env.Load();
 Env.TraversePath().Load();
 
+var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 var builder = WebApplication.CreateBuilder(args);
+
+// Get the environment
+var environment = builder.Environment.EnvironmentName; // Development, Staging, or Production
+
+// Conditional configuration
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine("Running in Development environment");
+}
+else if (builder.Environment.IsProduction()) {
+    Console.WriteLine("Running in Production environment");
+}
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDependencyInjection();
 builder.Services.ConfigureCorsPolicy();
-//builder.Services.AddDbContext<EfCoreContext>(option => option.UseSqlServer("server=" + Env.GetString("HOST") + ";user=" + Env.GetString("USERNAME") + ";password=" + Env.GetString("PASSWORD") + ";Database=" + Env.GetString("DATABASE") + ";Trusted_Connection=true;MultipleActiveResultSets=true;TrustServerCertificate=True"));
+builder.Services.AddDbContext<EfCoreContext>(option => option.UseSqlServer("server=" + Env.GetString("HOST") + ";user=" + Env.GetString("USERNAME") + ";password=" + Env.GetString("PASSWORD") + ";Database=" + Env.GetString("DATABASE") + ";Trusted_Connection=true;MultipleActiveResultSets=true;TrustServerCertificate=True"));
 
-builder.Services.AddDbContext<EfCoreContext>(option =>
-            option.UseSqlServer("server=localhost\\SQLEXPRESS2019; database=DB_Mcf; Integrated Security=true;TrustServerCertificate=True;")
-        );
+//builder.Services.AddDbContext<EfCoreContext>(option =>
+//            option.UseSqlServer("server=localhost\\SQLEXPRESS2019; database=DB_Mcf; Integrated Security=true;TrustServerCertificate=True;")
+//        );
+
 builder.Services.AddTransient<HandleExceptionMiddleware>();
 
 var app = builder.Build();
